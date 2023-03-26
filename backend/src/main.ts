268d83686fb705638efaba2d5cb20f613d9d6c34
mostafa-ashour <mostafa.ashour@vodafone.com>
2023-03-27 01:34:47 +0200

    g

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
     logger,
   });
 
-  console.log('hello from udapeopleffdddfff');
+  console.log('hello from udapeopleffffffdddfff');
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   const apiVersionPrefix: string = process.env.API_VERSION || 'api';
   app.setGlobalPrefix(apiVersionPrefix);
